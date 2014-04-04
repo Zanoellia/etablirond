e14fedaa132b4c6331cc50dd3c63ad0e3d0e52b3
Laurent Gourvenec <laurent.gourvenec@epita.fr>
2014-04-04 21:34:00 +0200

    paintsq() and erasecell() completed

--- peinture/commands.cc
+++ peinture/commands.cc
@@ -1,10 +1,16 @@
 #include "commands.hh"
 
+extern char entry[716][1522];
+
+/* Coordonées not checked */
 void paintsq(int r, int c, int s)
 {
+  for (int y = r - s; y <= r + s; y++)
+    for (int x = c - s; x <= c + s; x++)
+      entry[y][x] = '#';
 }
 
 void erasecell(int r, int c)
 {
+  entry[r][c] = '.';
 }
-
