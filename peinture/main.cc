a294582d31960dfb0f4b0a1e3797b9e4e819aa36
Antoine Cardon <antoine@toinews.net>
2014-04-04 22:21:04 +0200

    commit for Gourvy

--- peinture/main.cc
+++ peinture/main.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string.h>
 
 #include "entry.hh"
 #include "commands.hh"
@@ -6,16 +7,32 @@
 
 int main (int argc, char **argv)
 {
+	int count = 0;
+	memset(current, '.', 716*1523*sizeof(char));
 	for (int i = 0; i < 716 ; i++)
 	{
 		for (int j = 0; j < 1523 ; j++)
 		{
-			if (entry[i][j] == '#')
+			if (entry[i][j] == '#' && current[i][j] == '.')
 			{
-				paintsq(i, j, 0);
+				int counti = 1, countj = 1;
+				while (entry[i + counti][j] == '#' && current[i + counti][j] != '#') counti++;
+				while ((j + countj) < 716 && entry[i][j + countj] == '#'&& entry[i][j+ countj] != '#') countj++;
+				int min = std::min(counti, countj)/2;
+				paintsq(i + min, j + min, min);
+				count++;
+				std::cout << "PAINTSQ " << i + min << " " << j + min << " " <<  min << std::endl;
+
+			}
+			if (entry[i][j] == '.' && current[i][j] == '#')
+			{
+				erasecell(i, j);
+				std::cout << "ERASECELL " << i << " " << j << std::endl;
+				count++;
 			}
 		}
 	}
+	std::cout << count << std::endl;
 	return 0;
 }
 
