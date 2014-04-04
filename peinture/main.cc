c93871df15c4341cea683eb1e46697e5a2a8ad21
Antoine Cardon <antoine@toinews.net>
2014-04-04 21:31:56 +0200

    Dummy algo in main

--- peinture/main.cc
+++ peinture/main.cc
@@ -1,10 +1,20 @@
 #include <iostream>
 
 #include "entry.hh"
+#include "commands.hh"
 
 int main (int argc, char **argv)
 {
-
+	for (int i = 0; i < 716 ; i++)
+	{
+		for (int j = 0; j < 1523 ; j++)
+		{
+			if (entry[i][j] == '#')
+			{
+				paintsq(i, j, 1);
+			}
+		}
+	}
 	return 0;
 }
 
