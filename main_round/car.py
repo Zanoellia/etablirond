0497677ec0cb752da96a99686197d836a05fab2b
Antoine Cardon <antoine@toinews.net>
2014-04-05 22:48:36 +0200

    Added car structure

---
+++ main_round/car.py
@@ -0,0 +1,22 @@
+
+class Car:
+  currentNode = 0
+  time = 0
+  currentDistance = 0
+  path = []
+
+  def getCurrentNode():
+
+  def getPath():
+
+  def getDistance():
+
+  def getTime():
+
+  def addMove(node):
+
+
+
+
+
+
