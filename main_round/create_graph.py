b642d2d5b4f04276f9029d7e3bd7678956375109
Antoine Cardon <antoine@toinews.net>
2014-04-05 21:53:05 +0200

    Fix som bug in graph creation

--- main_round/create_graph.py
+++ main_round/create_graph.py
@@ -10,13 +10,13 @@ def CreateGraph(numNode, lEdge):
   while (i < numNode):
     G.add_node(i)
     i += 1
-  while (lEdge and not lEdge.empty()):
+  while (lEdge):
     (inter1, inter2, direction, cost, length) = lEdge.pop()
     G.add_edge(inter1,inter2, cost=cost, length=length)
     if (direction == 2):
       G.add_edge(inter2, inter1, cost=cost, length=length)
   return G
 
-G = CreateGraph(5, None)
+G = CreateGraph(5, [[0,1,1,5,5],[3,4,4,5,5]])
 nx.draw(G)
 plt.show()
